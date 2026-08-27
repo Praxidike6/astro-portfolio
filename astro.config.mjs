594b0d758aa6e0a9f4661057d62ccbff26956269
Stuart McDonnell <mcdonnell.stuart@gmail.com>
2026-08-27 16:15:42 +1000

    Improve SEO and social sharing metadata

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,9 +3,14 @@
 
 // https://astro.build/config
 import { defineConfig } from 'astro/config';
+import sitemap from '@astrojs/sitemap';
 
 export default defineConfig({
     site: 'https://www.usethefalseluke.com',
+
+    integrations: [
+        sitemap(),
+    ],
 
 });
 
