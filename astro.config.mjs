a76635d7f689af47ecf6d9b816311097049ff88f
Stuart McDonnell <mcdonnell.stuart@gmail.com>
2026-08-29 07:12:16 +1000

    Polish portfolio content and skills

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,12 +4,14 @@
 // https://astro.build/config
 import { defineConfig } from 'astro/config';
 import sitemap from '@astrojs/sitemap';
+import icon from 'astro-icon';
 
 export default defineConfig({
     site: 'https://www.usethefalseluke.com',
 
     integrations: [
         sitemap(),
+        icon(),
     ],
 
     build: {
